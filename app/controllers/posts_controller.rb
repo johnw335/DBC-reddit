5713afdc1d7541153f361a38cc9967740b6daaeb
Emily Owaki & John Williams <pair+jabbowaki.johnw335@devbootcamp.com>
2014-11-19 16:13:34 -0600

    got post controller to show and render view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,21 @@
 class PostsController < ApplicationController
-
   def index
 
   end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments.all
+    render :'post/show'
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
