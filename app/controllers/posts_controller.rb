47e7845ec4047e73501468d79e42bea78f940958
Emily Owaki & John Williams <pair+jabbowaki.johnw335@devbootcamp.com>
2014-11-19 16:44:57 -0600

    added delete, improved show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,17 +5,37 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comments.all
     render :'post/show'
   end
 
+  def new
+    @post = Post.new
+    render 'post/new'
+  end
+
   def create
+    @post = Post.new(post_params)
+    puts "======================================"
+    if @post.save
+      redirect_to subreddit_post_path(@post.subreddit, @post)
+    else
+      render 'post/new'
+    end
   end
 
   def update
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    subreddit = @post.subreddit
+    @post.destroy
+    redirect_to subreddit_path(subreddit)
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:author, :body, :title, :subreddit)
+    end
 
 end
