216f9773179427787dc577d7046af9e5761c781c
Alexander Antonow & Emily Owaki <pair+antonow.jabbowaki@devbootcamp.com>
2014-11-20 11:04:00 -0600

    fixing post controller bits

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -1,16 +1,11 @@
 class SubredditsController < ApplicationController
   def index
-<<<<<<< HEAD
     @subreddits = Subreddit.all
   end
 
   def show
     @subreddit = Subreddit.find(params[:id])
-
     @posts = @subreddit.posts
-=======
-    @subreddit = Subreddit.find(params[:subreddit_id])
->>>>>>> added delete, improved show
   end
 
   def new
