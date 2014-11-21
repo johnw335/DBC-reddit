24e6b121183b7cbeeeda2d9101caef4b8c719e76
Alexander Antonow <alexantonow@gmail.com>
2014-11-21 16:26:12 -0600

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,6 @@
 class CommentsController < ApplicationController
   def new
     @comment = Comment.new
-    respond_to do |format|
-      format.json {
-        render json: {
-          html: render(partial: "form", locals: {comment: @comment})
-        }
-      }
-
-      format.html{ render :new }
-    end
   end
 
   def create
