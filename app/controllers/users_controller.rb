ea8427a868b18bf800e381cc9a94fa8637857fd1
John Williams <jww335@gmail.com>
2014-11-21 13:12:41 -0600

    adding login, logout, and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,15 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url
+    else
+      flash.now[:error] = "Could not signup.  Please fill in all fields"
+      render 'new'
+    end
   end
 
   def edit
@@ -21,5 +30,10 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :username, :cohort, :password)
+    end
 
 end
