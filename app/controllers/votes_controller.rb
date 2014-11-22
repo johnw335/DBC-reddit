35e24b6f8189bf720ccef1b31abc053ddbb74cf9
Patrick Grenning <pgrennin@gmail.com>
2014-11-22 14:25:56 -0600

    Prevents user from voting if not logged in

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,33 +6,33 @@ class VotesController < ApplicationController
       @post = Post.find(params[:post_id])
     end
 
+
+    # if the user is not logged in when they vote, show error message.
+    if current_user == nil
+      flash[:vote_error] = true
+      redirect_to(:back)
+
     # if the vote is on a post do this:
-    if @post
+    elsif @post
       @vote = Vote.create(post: @post, up: params[:up])
       @vote.voter = current_user
       @subreddit = @post.subreddit
       @post.update(points: @post.net_votes)
-
       if params[:outside_post] == "true"
-        # redirect_to subreddit_path(@subreddit)
         redirect_to subreddit_path(@subreddit)
       else
         redirect_to subreddit_post_path(@subreddit, @post)
       end
+
     # if the vote is on a comment do this:
     elsif @comment
       @vote = Vote.create(comment: @comment, up: params[:up])
       @vote.voter = current_user
       @post = get_parent_comment(@comment).post
       @subreddit = @post.subreddit
-
       @comment.update(points: @comment.net_votes)
-
       redirect_to subreddit_post_path(@subreddit, @post)
     end
-  end
-
-  def destroy
   end
 
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,23 +1,29 @@
 require 'rails_helper'
 
 describe PostsController do
-  before do
-    @post = Post.create!(title: "Test", body: "test", author: User.all.sample, subreddit: Subreddit.all.sample)
-  end
-  # let!(:post) { Post.create!(title: "Test", body: "test", author: User.all.sample, subreddit: Subreddit.all.sample) }
+  let!(:post) {Post.create!(title: "Bicycles are awesome", body: "about bicycles")}
+
+  # puts "*********"
+  # puts @post.title
+
   describe "GET #show" do
     it "assigns @post equal to a particular post" do
-      get :show, { subreddit: @post.subreddit, post: @post }
+      get :show, { id: post.id }
       expect(assigns(:post)).to eq(post)
+      puts "-----------"
+      # expect(assigns(:post)).to be_a(Post)
     end
   end
 
-  describe "post#create" do
-    it "assigns the subreddit of the post to the post" do
-      # post :subreddit_posts
-      # expect(assigns(:post))
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "assigns the subreddit of the post to the post" do
+      end
     end
-
+    context "with invalid attributes" do
+      it "does not save the new post in the database" do
+      end
+    end
   end
 
 end
