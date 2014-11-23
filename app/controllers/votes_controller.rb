7d4221697cbf01c1ec828669e8225eea91693aea
John Williams <jww335@gmail.com>
2014-11-23 16:29:36 -0600

    fixed voting bug

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -17,16 +17,20 @@ class VotesController < ApplicationController
       @vote = Vote.create(post: @post, up: params[:up])
       @vote.voter = current_user
       # @subreddit = @post.subreddit
-      if @vote.save
+      if @vote.save && @vote.up == true
         @post.update(points: @post.points + 1)
+      elsif @vote.save && @vote.up == false
+        @post.update(points: @post.points - 1)
       end
 
     # if the vote is on a comment do this:
     elsif @comment
       @vote = Vote.create(comment: @comment, up: params[:up])
       @vote.voter = current_user
-      if @vote.save
+      if @vote.save && @vote.up == true
         @comment.update(points: @comment.points + 1)
+      elsif @vote.save && @vote.up == false
+        @comment.update(points: @comment.points - 1)
       end
       # @post = get_parent_comment(@comment).post
       # @subreddit = @post.subreddit
